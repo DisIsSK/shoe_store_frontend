089346644d2c624ae6716cc1727a77e542c8f08e
SANJAI KUMAR <sanjai199712@gmail.com>
2023-05-23 17:04:43 +0530

    initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,24 @@
+// import { Html, Head, Main, NextScript } from 'next/document'
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="..." />
+      </Head>
       <body>
         <Main />
         <NextScript />
